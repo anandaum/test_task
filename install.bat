21cd94e9e8eb2baef0a0a5451a7f25d8fdfa75a8
anandaum <anandaum@mail.ru>
2016-09-16 00:45:01 +0500

    Test task of Naumov Dmitriy with WordPress and Composer
    Composer assembles wordpress 4.5.3 and akismet plugin

---
+++ install.bat
@@ -0,0 +1,6 @@
+rem http://oddstyle.ru/wordpress-2/stati-wordpress/ispolzovanie-composer-v-wordpress-nebolshoe-rukovodstvo-dlya-novichkov.html
+rem http://oddstyle.ru/wordpress-2/stati-wordpress/upravlenie-wordpress-sajtom-s-pomoshhyu-git-i-composer-chast-2-ispolzuem-composer-dlya-upravleniya-temami-i-plaginami.html
+rem https://packagist.org/packages/johnpbloch/wordpress#4.5.3
+rem https://wpackagist.org
+
+php composer.phar install
